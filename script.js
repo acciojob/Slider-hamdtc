1e8f4bfe758705463dce33c88453f9f5495baece
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-04-22 21:55:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+ const slides = document.querySelectorAll('.slider-container');
+      let currentSlide = 0;
+      const nextSlide = () => {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+      };
+      const prevSlide = () => {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        slides[currentSlide].classList.add('active');
+      };
+      document.querySelector('.down-button').addEventListener('click', nextSlide);
+      document.querySelector('.up-button').addEventListener('click', prevSlide);
